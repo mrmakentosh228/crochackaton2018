41d6bf1d8db448b51a0208103b67f664b9c0034f
mrmakentosh228 <44507420+mrmakentosh228@users.noreply.github.com>
2018-10-27 13:24:25 +0300

    code

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,55 @@
+#define d1 A0
+#define d2 A1
+#define d3 A2
+#define d4 A3
+#define d5 A4
+#define d6 A5
+#define d7 A6
+#define d8 A7
+int ds[8] = {d1, d2, d3, d4, d5, d6, d7, d8};
+byte w = 0;
+byte b = 0;
+bool rl;
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (b == 8){
+    forwardAll(255);
+   }
+  else{
+    if rl{
+      forward(0);
+      forward(1);
+    }
+    else{
+      
+    }
+  }
+}
 
+
+void getVal(arr){
+  for (byte i, i < 8, i++){
+    val = analogRead(arr[i]);
+    if (val < 400){
+      w++;
+    }
+    else if (val > 400){
+      b++;
+    if (i == 0){
+      if (val < 400){
+        rl = True
+      }
+      else{
+        rl = False
+      }
+    }
+    }
+  }
 }
 
 void release() {
