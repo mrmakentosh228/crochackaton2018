c168fd27acca1f48bdd7e535fae7c493e7ad5001
Un ẞtrennen <n-i-k-i-t@yandex.ru>
2018-10-27 12:18:32 +0300

    исправил функции управления моторами

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,10 @@
+#define pwm_a 9
+#define pwm_b 10
+#define control_a_1 2
+#define control_a_2 3
+#define control_b_1 4
+#define control_b_2 5
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -8,22 +15,46 @@ void loop() {
 
 }
 
-void release() {
-  digitalWrite(control_1, LOW);
-  digitalWrite(control_2, LOW);
+void release(bool motor) {
+  if (!motor) {
+    digitalWrite(control_a_1, LOW);
+    digitalWrite(control_a_2, LOW);
+  }
+  else {
+    digitalWrite(control_b_1, LOW);
+    digitalWrite(control_b_2, LOW);
+  }
 }
 
-void brake() {
-  digitalWrite(control_1, HIGH);
-  digitalWrite(control_2, HIGH);
+void brake(bool motor) {
+  if (!motor) {
+    digitalWrite(control_a_1, HIGH);
+    digitalWrite(control_a_2, HIGH);
+  }
+  else {
+    digitalWrite(control_b_1, HIGH);
+    digitalWrite(control_b_2, HIGH);
+  }
 }
 
-void forward() {
-  digitalWrite(control_1, HIGH);
-  digitalWrite(control_2, LOW);
+void forward(bool motor) {
+  if (!motor) {
+    digitalWrite(control_a_1, HIGH);
+    digitalWrite(control_a_2, LOW);
+  }
+  else {
+    digitalWrite(control_b_1, HIGH);
+    digitalWrite(control_b_2, LOW);
+  }
 }
 
-void backrward() {
-  digitalWrite(control_1, LOW);
-  digitalWrite(control_2, HIGH);
+void backrward(bool motor) {
+  if (!motor) {
+    digitalWrite(control_a_1, LOW);
+    digitalWrite(control_a_2, HIGH);
+  }
+  else {
+    digitalWrite(control_b_1, LOW);
+    digitalWrite(control_b_2, HIGH);
+  }
 }
